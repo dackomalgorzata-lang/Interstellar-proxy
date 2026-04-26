d6dcda9b0b4b45f6e0eb9be5c36dc24a7c2ceb58
dackomalgorzata-lang <dackomalgorzata@gmail.com>
2026-04-26 21:29:46 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createBareServer } = require('bare-server-node');
+const { uvPath } = require('@titaniumnetwork-dev/ultraviolet');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const bare = createBareServer('/bare/');
+
+app.use('/uv/', express.static(uvPath));
+app.use(express.static(path.join(__dirname, 'static')));
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  if (bare.shouldRoute(req)) bare.routeRequest(req, res);
+  else app(req, res);
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bare.shouldRoute(req)) bare.routeUpgrade(req, socket, head);
+  else socket.end();
+});
+
+server.listen(process.env.PORT || 3000);
